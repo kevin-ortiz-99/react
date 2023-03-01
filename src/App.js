6c45a77daaf681e10883b0d6cf448d0b5cf33c4c
kevin-ortiz-99 <kevin.ortiz19@tectijuana.edu.mx>
2023-02-28 21:11:44 -0800

    react

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
 import logo from './logo.svg';
 import './App.css';
+import { Component } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+class App extends Component {
+  render(){
+    
+    const peliculas=[
+      { id: 1001, title: "Back to the Future"},
+      { id: 1652, title: "Home Alone", },
+      { id: 1734, title: "Titanic", },]
+  return ( 
+          
+    <div>
+      {peliculas.map((p,i)=><h1 key={i}>{p.title}</h1>)}
     </div>
-  );
+  );}
 }
 
 export default App;
